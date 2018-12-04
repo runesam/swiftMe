54c3cec57847d1b29ef8912b7d7cbe010fefd8ca
runesam <same7.hamada@gmail.com>
2018-12-04 08:50:42 +0100

    finish I am rich

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Semih Ewdala on 04.12.18.
+//  Copyright © 2018 bta3 computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
